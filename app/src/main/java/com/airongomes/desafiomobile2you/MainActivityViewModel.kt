a881f5daf15c51237e86a2bea22a2ce34990368e
Airon Gomes <airon.gms@gmail.com>
2021-03-27 10:37:00 -0300

    Initial ViewModel settings added.

--- app/src/main/java/com/airongomes/desafiomobile2you/MainActivityViewModel.kt
+++ app/src/main/java/com/airongomes/desafiomobile2you/MainActivityViewModel.kt
@@ -1,6 +1,38 @@
 package com.airongomes.desafiomobile2you
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.airongomes.desafiomobile2you.network.MovieInfo
+import com.airongomes.desafiomobile2you.repository.Repository
+import kotlinx.coroutines.launch
 
 class MainActivityViewModel: ViewModel() {
+
+    // LiveData Movie Information
+    private var _movieInfo = MutableLiveData<MovieInfo>()
+    val movieInfo : LiveData<MovieInfo>
+        get() = _movieInfo
+
+    init {
+        connectApi()
+    }
+
+    /**
+     * This function is responsible to use the repository class and require data by retrofit
+     */
+    private fun connectApi() {
+        viewModelScope.launch {
+            try {
+                val result = Repository().getMovieInfo()
+                _movieInfo.value = result
+                Log.d("Test", result.toString())
+
+            } catch (e: Exception) {
+                Log.d("Test", "Failure: ${e.message}")
+            }
+        }
+    }
 }
