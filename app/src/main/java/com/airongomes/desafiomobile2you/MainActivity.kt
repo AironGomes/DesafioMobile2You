ebe4078c16dd01c31899a3f198787401d1422c30
Airon Gomes <airon.gms@gmail.com>
2021-03-27 10:35:44 -0300

    Implementation of binding and viewModel.

--- app/src/main/java/com/airongomes/desafiomobile2you/MainActivity.kt
+++ app/src/main/java/com/airongomes/desafiomobile2you/MainActivity.kt
@@ -2,10 +2,31 @@ package com.airongomes.desafiomobile2you
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.airongomes.desafiomobile2you.databinding.ActivityMainBinding
+import com.airongomes.desafiomobile2you.repository.Repository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Create instance of viewModel
+        val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binding.viewModel = viewModel
+
+        viewModel.movieInfo.observe(this, Observer {
+            Log.d("Test", "viewmodel: $it")
+        })
+
+        binding.lifecycleOwner = this
     }
 }
+
