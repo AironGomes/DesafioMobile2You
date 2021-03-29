24c922e7f54b47cd8e064c03737ae2f94bef5920
Airon Gomes <airon.gms@gmail.com>
2021-03-29 11:02:45 -0300

    Added function getSimilarMovies.

--- app/src/main/java/com/airongomes/desafiomobile2you/repository/Repository.kt
+++ app/src/main/java/com/airongomes/desafiomobile2you/repository/Repository.kt
@@ -2,10 +2,24 @@ package com.airongomes.desafiomobile2you.repository
 
 import com.airongomes.desafiomobile2you.network.MovieInfo
 import com.airongomes.desafiomobile2you.network.Service.retrofitService
+import com.airongomes.desafiomobile2you.network.SimilarMovies
 
+/**
+ * Repository Class
+ */
 class Repository {
 
+    /**
+     * Request movie information by Retrofit
+     */
     suspend fun getMovieInfo(): MovieInfo {
         return retrofitService.getMovieInfo()
+    }
+
+    /**
+     * Request list of similar movies by Retrofit
+     */
+    suspend fun getSimilarMovies(): SimilarMovies {
+        return retrofitService.getSimilarMovies()
     }
 }
