ccef39c7844c5a0731baa4da4039071bad114506
Airon Gomes <airon.gms@gmail.com>
2021-03-29 11:05:41 -0300

    Created similarMovies livedata and request added.

--- app/src/main/java/com/airongomes/desafiomobile2you/MainActivityViewModel.kt
+++ app/src/main/java/com/airongomes/desafiomobile2you/MainActivityViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.airongomes.desafiomobile2you.network.MovieInfo
+import com.airongomes.desafiomobile2you.network.SimilarMovies
 import com.airongomes.desafiomobile2you.repository.Repository
 import kotlinx.coroutines.launch
 
@@ -15,6 +16,11 @@ class MainActivityViewModel: ViewModel() {
     private var _movieInfo = MutableLiveData<MovieInfo>()
     val movieInfo : LiveData<MovieInfo>
         get() = _movieInfo
+
+    // LiveData SimilarMovies
+    private var _similarMovies = MutableLiveData<SimilarMovies>()
+    val similarMovies : LiveData<SimilarMovies>
+        get() = _similarMovies
 
     init {
         connectApi()
@@ -26,9 +32,14 @@ class MainActivityViewModel: ViewModel() {
     private fun connectApi() {
         viewModelScope.launch {
             try {
-                val result = Repository().getMovieInfo()
-                _movieInfo.value = result
-                Log.d("Test", result.toString())
+                val resultMovieInfo = Repository().getMovieInfo()
+                _movieInfo.value = resultMovieInfo
+                //Log.d("Test", resultMovieInfo.toString())
+
+                val resultSimilarMovies = Repository().getSimilarMovies()
+                _similarMovies.value = resultSimilarMovies
+                Log.d("Test", resultSimilarMovies.toString())
+
 
             } catch (e: Exception) {
                 Log.d("Test", "Failure: ${e.message}")
