d9c3af32df9d19f8d7d18cb969cc94bbabff1f7b
Airon Gomes <airon.gms@gmail.com>
2021-03-29 20:03:25 -0300

    abbreviateValue updated to use Locale.US with DecimalFormat.

--- app/src/main/java/com/airongomes/desafiomobile2you/util/ConvertValues.kt
+++ app/src/main/java/com/airongomes/desafiomobile2you/util/ConvertValues.kt
@@ -2,26 +2,29 @@ package com.airongomes.desafiomobile2you.util
 
 import java.math.RoundingMode
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.*
 
 /**
  * Function used to abbreviate the number of likes
  */
 fun abbreviateValue(number: Double): String {
     return if(number < 1000.0) {
-        val df = DecimalFormat("#").apply {
+        val dfs = DecimalFormatSymbols(Locale.US)
+        val df = DecimalFormat("#", dfs).apply {
             roundingMode = RoundingMode.DOWN
         }
         df.format(number)
     }
     else {
-
-        val df = DecimalFormat("#.#").apply {
+        val dfs = DecimalFormatSymbols(Locale.US)
+        val df = DecimalFormat("#.#", dfs).apply {
             roundingMode = RoundingMode.DOWN
         }
         when (number) {
-            in 1001.0..999999.0 -> "${df.format(number/1000.0)}k"
-            in 1000000.0..999999999.0 -> "${df.format(number/1000000.0)}M"
-            else -> "${df.format(number/1000000000.0)}B"
+            in 1001.0..999999.0 -> "${df.format(number / 1000.0)}k"
+            in 1000000.0..999999999.0 -> "${df.format(number / 1000000.0)}M"
+            else -> "${df.format(number / 1000000000.0)}B"
         }
     }
 }
