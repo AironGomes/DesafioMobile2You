e5867c7795626e41a2a0ef147f17ec15580a7463
Airon Gomes <airon.gms@gmail.com>
2021-03-29 11:03:41 -0300

    SimilarMovieInfo data class added.

--- app/src/main/java/com/airongomes/desafiomobile2you/network/MovieInfo.kt
+++ app/src/main/java/com/airongomes/desafiomobile2you/network/MovieInfo.kt
@@ -6,5 +6,17 @@ data class MovieInfo (
     @Json(name = "original_title") val title: String,
     @Json(name = "backdrop_path") val pathImage: String,
     @Json(name = "vote_count") val likes: Int,
-    @Json(name = "popularity") val popularity: Double
-    )
+    @Json(name = "popularity") val popularity: Double,
+)
+
+data class SimilarMovieInfo (
+    @Json(name = "original_title") val title: String,
+    @Json(name = "backdrop_path") val pathImage: String,
+    @Json(name = "release_date") val date: String,
+    @Json(name = "genre_ids") val genreIds: List<Int>,
+)
+
+data class SimilarMovies(
+    @Json(name = "results") val similarMovies: List<SimilarMovieInfo>,
+)
+
